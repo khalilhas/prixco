b073401a3769df0550f7d4919fbb66b051671a5b
khalil <khalilhassebi@gmail.com>
2018-12-24 13:59:44 +0100

    delete item still in dev bugs exists

--- server.js
+++ server.js
@@ -63,8 +63,7 @@ app1.use(function(req , res, next ) {
               if(item == undefined){continue;}
               count++;
               rms += item.price;
-              console.log("append elements");
-              $('tbody').append("<tr><th scope='row'>" + count + "</th><td>"+ item.title +"</td><td>" + item.price +"</td><td><a href='"+ item.urldesc +"'>"+ item.location +"</a></td><td><a href=''><img class='deleteitem' src='./img/close.png'/></a></td></tr>");
+              $('tbody').append("<tr><th scope='row'>" + count + "</th><td>"+ item.title +"</td><td>" + item.price +"</td><td><a href='"+ item.urldesc +"'>"+ item.location +"</a></td><td class='deleteitem'><img src='./img/close.png'/></td></tr>");
             }
             // calculate and display the changes
             $('#rms').text("Moyenne: "+ (rms / json.item.length).toFixed(2) +" dh");
